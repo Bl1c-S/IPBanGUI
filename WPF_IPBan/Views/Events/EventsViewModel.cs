9e54ec8a3ef44f34ad728d9535bd53f8ff0014c7
Олександр Кубарич <kuseas5@gmail.com>
2024-01-29 21:48:22 +0200

    Add(LogView:1): Додано вікно перегляду логів EventsView
    До ServiceProvider додано LogEventManager, EventsViewModel.
    
    ##Додатково рефактор та зміна логіки:
    
    ###NavigationService:
    Додано ресурси для назв сторінок.
    Для VM сторінок змінено базовий клас на PageViewModel
    Змінено принцип навігації по сторінкам, та візуальні зміни навігації
    Замінено конвертор для навігації.
    
    ###Додатково:
    Змінено назву вікна MainWindow (Версію).
    Змінено імя методу для LogEventManager.
    Зміна логіки для методу LogEventBuilder.

--- WPF_IPBan/Views/Events/EventsViewModel.cs
+++ WPF_IPBan/Views/Events/EventsViewModel.cs
@@ -1,5 +1,26 @@
-﻿namespace WPF_IPBanUtility;
+﻿using Logic_IPBanUtility.Logic.LogFile;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
-internal class EventsViewModel : ViewModelBase
+namespace WPF_IPBanUtility;
+
+internal class EventsViewModel : PageViewModel
 {
+     public LogEventManager LogManager { get; }
+     public ObservableCollection<LogEvent> LogEvents { get; set; }
+     public bool LogEventIsEmpty => LogEvents.Count == 0;
+
+     public List<ViewModelBase> VMs => _vMs;
+     private List<ViewModelBase> _vMs;
+
+     public EventsViewModel(LogEventManager logManager) : base(Properties.PageName.Events)
+     {
+          LogManager = logManager;
+          LogEvents = new(LogManager.ReadAllLogEvents());
+
+          _vMs = new() {
+               new LogEventListViewModel(LogEvents)
+          };
+     }
+
 }
