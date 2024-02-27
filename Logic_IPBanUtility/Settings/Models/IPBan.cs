f6886be687e18e628868ca0cc446ca293167e4e0
Олександр Кубарич <kuseas5@gmail.com>
2024-02-27 16:39:50 +0200

    Add: Для IPBan додано LogFilePathExtractor Виконує пошук файлів з логами та визначення за яку дату вони відповідають. Додано можоивість отримати список шляхів до файлу з логами за відповідний день GetDaysWithLogFilePath

--- Logic_IPBanUtility/Settings/Models/IPBan.cs
+++ Logic_IPBanUtility/Settings/Models/IPBan.cs
@@ -1,7 +1,9 @@
-﻿namespace Logic_IPBanUtility;
+﻿using Logic_IPBanUtility.Setting.Builders;
+
+namespace Logic_IPBanUtility;
 public class IPBan
 {
-     private const string _NAME_LogFile = "logFile."; //Використовується для пошуку всіх файлів
+     private readonly LogFilePathExtractor _logsExtractor;
      private const string _NAME_IPBan = "ipban.config";
      public string Folder { get; set; }
      public string Context { get; set; }
@@ -12,17 +14,21 @@ public class IPBan
           Folder = folder;
           Context = context;
           Logfile = logfile;
+          _logsExtractor = new(Folder);
      }
 
      public static IPBan Create(string iPBanFolderPath)
      {
           var context = Path.Combine(iPBanFolderPath, _NAME_IPBan);
-          var logfile = Path.Combine(iPBanFolderPath, _NAME_LogFile + "txt");
+          var logExtractor = new LogFilePathExtractor(iPBanFolderPath);
+          var logfile = logExtractor.ToDatLogFilePath;
 
           var iPBan = new IPBan(iPBanFolderPath, context, logfile);
           iPBan.CheckExist();
           return iPBan;
      }
+
+     public Dictionary<DateTime, string> GetDaysWithLogFilePath() => _logsExtractor.GetDaysWithLogFilePath();
 
      public bool CheckExist()
      {
@@ -34,17 +40,5 @@ public class IPBan
           if (!File.Exists(Logfile))
                throw new FileNotFoundException(m + Logfile);
           return true;
-     }
-
-     public string[] GetAllLogFilePaths()
-     {
-          var allFiles = Directory.GetFiles(Folder);
-          var logFiles = new List<string>();
-
-          foreach (var file in allFiles)
-               if (file.Contains(_NAME_LogFile))
-                    logFiles.Add(file);
-
-          return logFiles.ToArray();
      }
 }
