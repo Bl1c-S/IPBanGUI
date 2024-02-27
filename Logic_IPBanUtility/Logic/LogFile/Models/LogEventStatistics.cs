0dc0b3e9c9d5945c38e3aa33e693d0995432c49d
Олександр Кубарич <kuseas5@gmail.com>
2024-02-27 16:49:58 +0200

    Refactor: LogEventStatistics змінено принцип запису статистики. Додано можливість скидання записаної статистики.

--- Logic_IPBanUtility/Logic/LogFile/Models/LogEventStatistics.cs
+++ Logic_IPBanUtility/Logic/LogFile/Models/LogEventStatistics.cs
@@ -1,7 +1,8 @@
-﻿namespace Logic_IPBanUtility.Logic.LogFile.Models;
+﻿namespace Logic_IPBanUtility.Logic.LogFile;
 
 public class LogEventStatistics
 {
+     public Action? StatisticsChanged;
      public int AllLogEvent { get; private set; }
      public int LoginSucceeded { get; private set; }
      public int LoginFailure { get; private set; }
@@ -10,42 +11,55 @@ public class LogEventStatistics
      public int UnBanningIP { get; private set; }
      public int FirewallEntriesUpdated { get; private set; }
 
-     public void AddOneEvent(LogEventType type)
+     public void AddEvents(LogEventType type, int count = 1)
      {
-          ++AllLogEvent;
+          AllLogEvent += count;
           switch (type)
           {
                case LogEventType.LoginSucceeded:
-                    ++LoginSucceeded; break;
+                    LoginSucceeded += count; break;
                case LogEventType.LoginFailure:
-                    ++LoginFailure; break;
+                    LoginFailure += count; break;
                case LogEventType.ForgetFailedLogin:
-                    ++ForgetFailedLogin; break;
+                    ForgetFailedLogin += count ; break;
                case LogEventType.BanningIP:
-                    ++BanningIP; break;
+                    BanningIP += count ; break;
                case LogEventType.UnBanningIP:
-                    ++UnBanningIP; break;
+                    UnBanningIP += count; break;
                case LogEventType.FirewallEntriesUpdated:
-                    ++FirewallEntriesUpdated; break;
+                    FirewallEntriesUpdated += count; break;
           }
+          StatisticsChanged?.Invoke();
      }
-     public void RemoveOneEvent(LogEventType type)
+     public void RemoveEvents(LogEventType type, int count = 1)
      {
-          --AllLogEvent;
+          //AllLogEvent -= count; З списку всіх логів ми ніколи не видаляємо обєкти, тому й не потрібно змінювати число всіх.
           switch (type)
           {
                case LogEventType.LoginSucceeded:
-                    --LoginSucceeded; break;
+                    LoginSucceeded -= count; break;
                case LogEventType.LoginFailure:
-                    --LoginFailure; break;
+                    LoginFailure -= count; break;
                case LogEventType.ForgetFailedLogin:
-                    --ForgetFailedLogin; break;
+                    ForgetFailedLogin -= count; break;
                case LogEventType.BanningIP:
-                    --BanningIP; break;
+                    BanningIP -= count; break;
                case LogEventType.UnBanningIP:
-                    --UnBanningIP; break;
+                    UnBanningIP -= count; break;
                case LogEventType.FirewallEntriesUpdated:
-                    --FirewallEntriesUpdated; break;
+                    FirewallEntriesUpdated -= count; break;
           }
+          StatisticsChanged?.Invoke();
+     }
+     public void Clear()
+     {
+          AllLogEvent = 0;
+          LoginSucceeded = 0;
+          LoginFailure = 0;
+          ForgetFailedLogin = 0;
+          BanningIP = 0;
+          UnBanningIP = 0;
+          FirewallEntriesUpdated = 0;
+          StatisticsChanged?.Invoke();
      }
 }
