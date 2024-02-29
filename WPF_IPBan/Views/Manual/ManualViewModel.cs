22c39a2cca5e0604e6b5443608e8cc87d52c7891
Олександр Кубарич <kuseas5@gmail.com>
2024-02-29 11:00:32 +0200

    Add: Додано вбудовану інструкцію з використання застосунку. Додано інструкцію по оновленню програми. Додано ілюстрації для інструкцій.

--- WPF_IPBan/Views/Manual/ManualViewModel.cs
+++ WPF_IPBan/Views/Manual/ManualViewModel.cs
@@ -5,10 +5,26 @@ namespace WPF_IPBanUtility;
 internal class ManualViewModel : PageViewModelBase
 {
      public string WelcomeParagraph { get => Manual.WelcomeParagraph; }
-     public string Paragraph1 { get => Manual.Paragraph1; }
-     public SupportViewModel SupportVM { get; set; }
+
+     public string KeyTitle { get => PageNames.KeyList; }
+     public string KeyParagraph1 { get => Manual.KeyParagraph1; }
+     public string KeyParagraph2 { get => Manual.KeyParagraph2; }
+
+     public string EventTitle { get => PageNames.Events; }
+     public string EventParagraph1 { get => Manual.EventParagraph1; }
+     public string EventParagraph2 { get => Manual.EventParagraph2; }
+
+     public string SupportTitle { get => PageNames.Support; }
+     public string SupportDescription { get => Messages.SupportDescription; }
+     public string SupportMail { get => "https://mail.google.com/mail/u/0/?tab=rm&ogbl#search/kubarych.torgsoft%40gmail.com?compose=new"; }
+     public string GitHubDiscussions { get => "https://github.com/Bl1c-S/IPBanGUI/discussions"; }
+
+     public string UpdateTitle { get => PageNames.UpdateTitle; }
+     public string UpdateParagraph1 { get => Manual.UpdateParagraph1; }
+     public string UpdateParagraph2 { get => Manual.UpdateParagraph2; }
+     public string Updates { get => "https://github.com/Bl1c-S/IPBanGUI/releases"; }
+
      public ManualViewModel() : base(PageNames.Manual)
      {
-          SupportVM = new SupportViewModel();
      }
 }
