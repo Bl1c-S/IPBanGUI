2607ce1cea484562a035309e762c0f54e97a65b1
Олександр Кубарич <kuseas5@gmail.com>
2024-04-16 15:22:23 +0300

    Refactor: Змінено структуру LogEventStatistics. Без змін API.

--- Logic_IPBanUtility/Logic/LogFile/Models/LogEventStatistics.cs
+++ Logic_IPBanUtility/Logic/LogFile/Models/LogEventStatistics.cs
@@ -3,63 +3,43 @@
 public class LogEventStatistics
 {
      public Action? StatisticsChanged;
-     public int AllLogEvent { get; private set; }
-     public int LoginSucceeded { get; private set; }
-     public int LoginFailure { get; private set; }
-     public int ForgetFailedLogin { get; private set; }
-     public int BanningIP { get; private set; }
-     public int UnBanningIP { get; private set; }
-     public int FirewallEntriesUpdated { get; private set; }
+     private Dictionary<LogEventType, int> statistics;
+     public LogEventStatistics()
+     {
+          statistics = Enum.GetValues(typeof(LogEventType))
+        .Cast<LogEventType>()
+        .ToDictionary(key => key, value => 0);
+     }
+     public int AllLogEvent => GetAll();
+     public int LoginSucceeded => Get(LogEventType.LoginSucceeded);
+     public int LoginFailure => Get(LogEventType.LoginFailure);
+     public int ForgetFailedLogin => Get(LogEventType.ForgetFailedLogin);
+     public int BanningIP => Get(LogEventType.BanningIP);
+     public int UnBanningIP => Get(LogEventType.UnBanningIP);
+     public int FirewallEntriesUpdated => Get(LogEventType.FirewallEntriesUpdated);
 
      public void AddEvents(LogEventType type, int count = 1)
      {
-          AllLogEvent += count;
-          switch (type)
-          {
-               case LogEventType.LoginSucceeded:
-                    LoginSucceeded += count; break;
-               case LogEventType.LoginFailure:
-                    LoginFailure += count; break;
-               case LogEventType.ForgetFailedLogin:
-                    ForgetFailedLogin += count ; break;
-               case LogEventType.BanningIP:
-                    BanningIP += count ; break;
-               case LogEventType.UnBanningIP:
-                    UnBanningIP += count; break;
-               case LogEventType.FirewallEntriesUpdated:
-                    FirewallEntriesUpdated += count; break;
-          }
+          statistics[type] += count;
           StatisticsChanged?.Invoke();
      }
      public void RemoveEvents(LogEventType type, int count = 1)
      {
-          //AllLogEvent -= count; З списку всіх логів ми ніколи не видаляємо обєкти, тому й не потрібно змінювати число всіх.
-          switch (type)
-          {
-               case LogEventType.LoginSucceeded:
-                    LoginSucceeded -= count; break;
-               case LogEventType.LoginFailure:
-                    LoginFailure -= count; break;
-               case LogEventType.ForgetFailedLogin:
-                    ForgetFailedLogin -= count; break;
-               case LogEventType.BanningIP:
-                    BanningIP -= count; break;
-               case LogEventType.UnBanningIP:
-                    UnBanningIP -= count; break;
-               case LogEventType.FirewallEntriesUpdated:
-                    FirewallEntriesUpdated -= count; break;
-          }
+          statistics[type] -= count;
           StatisticsChanged?.Invoke();
      }
      public void Clear()
      {
-          AllLogEvent = 0;
-          LoginSucceeded = 0;
-          LoginFailure = 0;
-          ForgetFailedLogin = 0;
-          BanningIP = 0;
-          UnBanningIP = 0;
-          FirewallEntriesUpdated = 0;
+          foreach (var key in statistics.Keys.ToList())
+               statistics[key] = 0;
           StatisticsChanged?.Invoke();
+     }
+     public int Get(LogEventType type) => statistics[type];
+     private int GetAll()
+     {
+          int all = 0;
+          foreach (var value in statistics.Values)
+               all += value;
+          return all;
      }
 }
