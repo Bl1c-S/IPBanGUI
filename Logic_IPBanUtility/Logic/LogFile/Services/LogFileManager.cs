321a77fcebfac08fcae112aa600a797c53bbb783
Олександр Кубарич <kuseas5@gmail.com>
2024-04-16 15:25:28 +0300

    Refactor: LogFileManager change to public LogFilePath. Видалено не використовуваний метод.

--- Logic_IPBanUtility/Logic/LogFile/Services/LogFileManager.cs
+++ Logic_IPBanUtility/Logic/LogFile/Services/LogFileManager.cs
@@ -9,23 +9,21 @@ public class LogFileManager
      private readonly StreamFileManager _streamFileManager = new();
      private readonly LogEventBuilder _logEventBuilder = new();
 
-     private readonly string _logFilePath;
+     public readonly string LogFilePath;
      private int _lastLogEventId;
 
      public LogFileManager(string logFilePath)
      {
-          _logFilePath = logFilePath;
+          LogFilePath = logFilePath;
      }
      public List<LogEvent> ReadNewLogEvents(bool readFirst = false)
      {
           var firstLogID = readFirst ? 0 : _lastLogEventId;
-          var newLogs = _streamFileManager.StreamReadAllNewLines(_logFilePath, readFirst);
+          var newLogs = _streamFileManager.StreamReadAllNewLines(LogFilePath, readFirst);
           var newLogEvents = _logEventBuilder.GetLogEvents(newLogs, firstLogID + 1);
 
           _lastLogEventId += newLogEvents.Count;
           LogEventsChanged?.Invoke();
           return newLogEvents;
      }
-
-     public void DeleteLogFile() => File.Delete(_logFilePath);
 }
