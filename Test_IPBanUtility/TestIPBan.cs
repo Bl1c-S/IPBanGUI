cfbc3a6cdcf537b6e0525f51930b5deb608bf2a3
Олександр Кубарич <kuseas5@gmail.com>
2024-04-29 11:31:34 +0300

    Test: Додано сервіс для створення тестового IPBan об'єкта.

---
+++ Test_IPBanUtility/TestIPBan.cs
@@ -0,0 +1,22 @@
+﻿namespace Test_IPBanUtility;
+
+public class TestIPBan
+{
+     private readonly string _folder;
+     public TestIPBan(string customFolder)
+     {
+          _folder = AppDomain.CurrentDomain.BaseDirectory + customFolder;
+     }
+
+     public IPBan CreateEmptyIPBan()
+     {
+          CreateEmptyFiles("ipban.config");
+          CreateEmptyFiles("ipban.sqlite");
+          return IPBan.Create(_folder);
+     }
+     private void CreateEmptyFiles(string name)
+     {
+          var path = Path.Combine(_folder, name);
+          if (!File.Exists(path)) File.Create(path);
+     }
+}
