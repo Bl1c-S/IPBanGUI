6f7123d60e4a2d18b20714ef75ffd16b124fb594
Олександр Кубарич <kuseas5@gmail.com>
2024-05-01 15:55:25 +0300

    Add: Створено голосний сервіс керування заблокованими адресами IPBlockedListService.

---
+++ Logic_IPBanUtility/Logic/IPList/IPBlockedListService.cs
@@ -0,0 +1,36 @@
+﻿using Logic_IPBanUtility.Logic.ConfigFile;
+using Logic_IPBanUtility.Models;
+
+namespace Logic_IPBanUtility.Logic.IPList;
+
+public class IPBlockedListService
+{
+     public List<IPAddressEntity> IPs => _iPManager.IPAddress;
+     public Action? IPsChanged { get => _iPManager.IPAddressChanged; set => _iPManager.IPAddressChanged = value; }
+
+     private readonly IPAddressManager _iPManager;
+     private readonly KeyValueManager _keyManager;
+
+     public IPBlockedListService(KeyValueManager keyManager, IPAddressManager iPAddressManager)
+     {
+          _keyManager = keyManager;
+          _iPManager = iPAddressManager;
+     }
+
+     public void Add(IPAddressEntity ip) => _iPManager.Add(ip); //For tests
+     public void Remove(IPAddressEntity ip) => _iPManager.Remove(ip);
+     public void RemoveAll() => _iPManager.RemoveAll();
+
+     #region Keys
+     public void AddToWhiteList(IPAddressEntity ip)
+     {
+          Remove(ip);
+          _keyManager.AddIpToKey(KeyNames.Whitelist, ip.IPAddressText);
+     }
+     public void AddToBlacklist(IPAddressEntity ip)
+     {
+          Remove(ip);
+          _keyManager.AddIpToKey(KeyNames.Blacklist, ip.IPAddressText);
+     }
+     #endregion
+}
