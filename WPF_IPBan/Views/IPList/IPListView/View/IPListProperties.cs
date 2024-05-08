6c6ac196dda5eaa0db1dbdeb194a87e769277810
Олександр Кубарич <kuseas5@gmail.com>
2024-05-09 00:32:54 +0300

    Add: Додано IPListViewProperties Для зберігання стану відображення списків адрес.

---
+++ WPF_IPBan/Views/IPList/IPListView/View/IPListProperties.cs
@@ -0,0 +1,11 @@
+﻿using WPF_IPBanUtility.Views.IPList;
+
+namespace WPF_IPBanUtility
+{
+     public class IPListProperties
+     {
+          public IPListViewProperties BlockList = new(true);
+          public IPListViewProperties WhiteList = new(false);
+          public IPListViewProperties BlackList = new(false);
+     }
+}
---
+++ WPF_IPBan/Views/IPList/Views/Base/IPListViewProperties.cs
@@ -0,0 +1,36 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.ComponentModel;
+using System.Windows;
+using Wpf.Ui.Common;
+
+namespace WPF_IPBanUtility.Views.IPList
+{
+    public class IPListViewProperties : ObservableObject
+     {
+          private bool isHide;
+          public Visibility ListVisibility { get; private set; }
+          public SymbolRegular ListIcon { get; private set; }
+
+          public IPListViewProperties(bool ListVisibility)
+          {
+               isHide = ListVisibility;
+               Hide();
+          }
+          public void Hide()
+          {
+               if (!isHide)
+               {
+                    ListVisibility = Visibility.Collapsed;
+                    ListIcon = SymbolRegular.ChevronUp24;
+               }
+               else
+               {
+                    ListVisibility = Visibility.Visible;
+                    ListIcon = SymbolRegular.ChevronDown24;
+               }
+               isHide = !isHide;
+               OnPropertyChanged(nameof(ListVisibility));
+               OnPropertyChanged(nameof(ListIcon));
+          }
+     }
+}
