919e251b8031b6b0651e771f894e90e94c81fe34
Олександр Кубарич <kuseas5@gmail.com>
2024-05-10 00:00:22 +0300

    Fix: Оновлено методи Додавання та видалення адреси з БД. Додано оновлення таблиці перед пошуком обэкту для видалення. Під час видалення, час блокування може минути.

--- Logic_IPBanUtility/Logic/IPList/IPAddressManager/Services/IPAddressDatabaseManager.cs
+++ Logic_IPBanUtility/Logic/IPList/IPAddressManager/Services/IPAddressDatabaseManager.cs
@@ -1,72 +1,71 @@
 ﻿using Logic_IPBanUtility.Setting;
+using Microsoft.EntityFrameworkCore;
 
 namespace Logic_IPBanUtility.Logic.IPList;
 
 public class IPAddressDatabaseManager
 {
-    private string _path;
-    public IPAddressDatabaseManager(Settings settings)
-    {
-        _path = settings.IPBan.Sqlite_db;
-    }
-    public void Add(IPAddressEntity entity)
-    {
-        using (var dbContext = new IPAddressesDbContext(_path))
-        {
-            try
-            {
-                dbContext.Add(entity);
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка додавання {entity.IPAddressText} до бази даних {_path}. \n\r {ex.Message}");
-            }
-        }
-    }
-    public List<IPAddressEntity> GetAll()
-    {
-        using (var dbContext = new IPAddressesDbContext(_path))
-        {
-            try
-            {
-                var ips = dbContext.GetAll();
-                return ips;
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка підключення до бази даних {_path}. \n\r {ex.Message}");
-            }
-        }
-    }
-    public void Remove(IPAddressEntity entity)
-    {
-        using (var dbContext = new IPAddressesDbContext(_path))
-        {
-            try
-            {
-                dbContext.Remove(entity.IPAddressText);
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка видалення {entity.IPAddressText} з бази {_path}. \n\r {ex.Message}");
-            }
-        }
-    }
-    public void RemoveAll()
-    {
-        using (var dbContext = new IPAddressesDbContext(_path))
-        {
-            try
-            {
-                dbContext.RemoveAll();
-                dbContext.SaveChanges();
-            }
-            catch (Exception ex)
-            {
-                throw new Exception($"Помилка видалення всіх заблокованих ІР з бази {_path}. \n\r {ex.Message}");
-            }
-        }
-    }
+     private string _path;
+     public IPAddressDatabaseManager(Settings settings)
+     {
+          _path = settings.IPBan.Sqlite_db;
+     }
+     public void Add(IPAddressEntity entity)
+     {
+          using (var dbContext = new IPAddressesDbContext(_path))
+          {
+               try
+               {
+                    dbContext.Add(entity);
+               }
+               catch (Exception ex)
+               {
+                    throw new Exception($"Помилка додавання {entity.IPAddressText} до бази даних {_path}. \n\r {ex.Message}");
+               }
+          }
+     }
+     public List<IPAddressEntity> GetAll()
+     {
+          using (var dbContext = new IPAddressesDbContext(_path))
+          {
+               try
+               {
+                    var ips = dbContext.GetAll();
+                    return ips;
+               }
+               catch (Exception ex)
+               {
+                    throw new Exception($"Помилка підключення до бази даних {_path}. \n\r {ex.Message}");
+               }
+          }
+     }
+     public void Remove(IPAddressEntity entity)
+     {
+          using (var dbContext = new IPAddressesDbContext(_path))
+          {
+               try
+               {
+                    dbContext.Remove(entity.IPAddressText);
+               }
+               catch (Exception ex)
+               {
+                    throw new Exception($"Помилка видалення {entity.IPAddressText} з бази {_path}. \n\r {ex.Message}");
+               }
+          }
+     }
+     public void RemoveAll()
+     {
+          using (var dbContext = new IPAddressesDbContext(_path))
+          {
+               try
+               {
+                    dbContext.RemoveAll();
+                    dbContext.SaveChanges();
+               }
+               catch (Exception ex)
+               {
+                    throw new Exception($"Помилка видалення всіх заблокованих ІР з бази {_path}. \n\r {ex.Message}");
+               }
+          }
+     }
 }
--- Logic_IPBanUtility/Logic/IPList/IPAddressManager/Services/IPAddressesDbContext.cs
+++ Logic_IPBanUtility/Logic/IPList/IPAddressManager/Services/IPAddressesDbContext.cs
@@ -1,40 +1,46 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace Logic_IPBanUtility.Logic.IPList;
 
 public class IPAddressesDbContext : DbContext
 {
-    private string _path;
-    public IPAddressesDbContext(string path) => _path = path;
+     private string _path;
+     public IPAddressesDbContext(string path) => _path = path;
 
-    public DbSet<IPAddressEntityDTO> IPAddresses { get; set; }
+     public DbSet<IPAddressEntityDTO> IPAddresses { get; set; }
 
-    public void Add(IPAddressEntity entity)
-    {
-        var dto = IPAddressEntityDTO.ToDTO(entity);
-        IPAddresses.Add(dto);
-    }
-    public void Remove(string iPAddressText)
-    {
-        var targetDTO = IPAddresses.FirstOrDefault(x => x.IPAddressText == iPAddressText);
-        if (targetDTO != null)
-            IPAddresses.Remove(targetDTO);
-    }
-    public void RemoveAll()
-    {
-        foreach (var ip in IPAddresses)
-            Remove(ip);
-    }
-    public void LoadTable() => IPAddresses.Load();
-    public List<IPAddressEntity> GetAll()
-    {
-        var dto = IPAddresses.ToList();
-        return Convert(dto);
-    }
+     public void Add(IPAddressEntity entity)
+     {
+          var dto = IPAddressEntityDTO.ToDTO(entity);
+          IPAddresses.Add(dto);
+          SaveChanges();
+     }
+     public void Remove(string iPAddressText)
+     {
+          LoadTable();
+          var targetDTO = IPAddresses.FirstOrDefault(x => x.IPAddressText == iPAddressText);
+          if (targetDTO != null)
+          {
+               IPAddresses.Remove(targetDTO);
+               SaveChanges();
+          }
+     }
+     public void RemoveAll()
+     {
+          foreach (var ip in IPAddresses)
+               Remove(ip);
+     }
+     public void LoadTable() => IPAddresses.Load();
+     public List<IPAddressEntity> GetAll()
+     {
+          var dto = IPAddresses.ToList();
+          return Convert(dto);
+     }
 
-    private List<IPAddressEntity> Convert(List<IPAddressEntityDTO> addressesDTO) => addressesDTO.Select(dto => dto.ToEntity()).ToList();
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite($"Data Source={_path}");
-    }
+     private List<IPAddressEntity> Convert(List<IPAddressEntityDTO> addressesDTO) => addressesDTO.Select(dto => dto.ToEntity()).ToList();
+     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+     {
+          optionsBuilder.UseSqlite($"Data Source={_path}");
+     }
 }
