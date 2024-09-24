66b96e712c84d0f3efa288620bb80715dfab745c
Олександр Кубарич <kuseas5@gmail.com>
2024-09-24 11:21:15 +0300

    Fix: (IPAddressManager) Виправлено порівняння двох списків адрес. - Видалено надлишкову кількість викликів оновлення.

--- Logic_IPBanUtility/Logic/IPList/IPAddressManager/IPAddressManager.cs
+++ Logic_IPBanUtility/Logic/IPList/IPAddressManager/IPAddressManager.cs
@@ -29,29 +29,35 @@ namespace Logic_IPBanUtility.Logic.IPList
                     IPAddressChanged?.Invoke();
                }
           }
+
           public void Update()
           {
-               var newIPAddressList = _dBManager.GetAll();
-               if (!IPAddress.SequenceEqual(newIPAddressList))
-               {
-                    IPAddress = newIPAddressList;
-                    IPAddressChanged?.Invoke();
-               }
+               if (!IPsEqual()) IPAddressChanged?.Invoke();
           }
+          private bool IPsEqual()
+          {
+               var newIPAddressList = _dBManager.GetAll();
+               if (newIPAddressList.Count != IPAddress.Count) return false;
+
+               foreach (var newIp in newIPAddressList)
+               {
+                    var ip = IPAddress.FirstOrDefault(ip => ip.IPAddressText == newIp.IPAddressText);
+                    if (ip == null) return false;
+               }
+
+               IPAddress = newIPAddressList;
+               return true;
+          }
+          private bool Equal(IPAddressEntity oldIP, IPAddressEntity newIP)
+          {
+               return oldIP.IPAddressText == newIP.IPAddressText;
+          }
+
           public void Remove(IPAddressEntity iPAddress)
           {
-               Update();
                var ip = IPAddress.Find(x => x.IPAddressText == iPAddress.IPAddressText)!;
 
                if (ip != null) AddToUnBan(ip);
-          }
-
-          private void AddToUnBan(IPAddressEntity ip)
-          {
-               IPAddress.Remove(ip);
-               _dBManager.Remove(ip);
-               IPAddressChanged?.Invoke();
-               _unBanService.Add(ip.IPAddressText);
           }
 
           public void RemoveAll()
@@ -61,12 +67,18 @@ namespace Logic_IPBanUtility.Logic.IPList
 
                _dBManager.RemoveAll();
                IPAddress.Clear();
-               IPAddressChanged?.Invoke();
           }
 
           public void ApplyRemove()
           {
                _unBanService.CreateFile();
+          }
+
+          private void AddToUnBan(IPAddressEntity ip)
+          {
+               IPAddress.Remove(ip);
+               _dBManager.Remove(ip);
+               _unBanService.Add(ip.IPAddressText);
           }
 
           private bool IsNewIP(IPAddressEntity iPAddress)
